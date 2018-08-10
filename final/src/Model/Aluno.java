489700abab8bbb140090e4a0240ce0eed3257cf6
ifnmg <ifnmg@ifnmg-HP-EliteDesk-705-G1-SFF>
2018-08-10 20:42:23 +0000

    Classes

---
+++ final/src/Model/Aluno.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author ifnmg
+ */
+public class Aluno extends Pessoa{
+    private Responsavel responsavel;
+    private String matricula;
+    private Integer responsavel_cpf;
+    private Integer turma_idturma;
+
+    public Responsavel getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(Responsavel responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
